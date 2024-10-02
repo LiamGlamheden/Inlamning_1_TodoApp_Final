f99b93340d7b4f087bd7c0a670448d813e404282
Liam <04liagla@skola.boras.se>
2024-10-03 00:08:00 +0200

    added TextInput for name of task

--- Screens/CreateTodoScreen.tsx
+++ Screens/CreateTodoScreen.tsx
@@ -1,14 +1,46 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Text } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { RootStackParamList } from '../App';
+import React from 'react';
 
 type Props = NativeStackScreenProps<RootStackParamList, "CreateTodo">;
 
 
 export default function CreateTodoScreen(){
+     const [text, onChangeText] = React.useState('');
+     const [number, onChangeNumber] = React.useState('');
     return(
-         <Text>
-            CreateTodoScreen
-        </Text>
+        <View>
+            <Text>
+                CreateTodoScreen
+            </Text>
+
+            <TextInput
+             style={styles.input}
+            placeholder="Task name"
+            onChangeText={onChangeText}
+            value={text}
+            />
+
+         
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  title: {
+    color: 'black',
+  },
+});
