8fb5d4e02896bf58b9754077ad1a972a1567fa83
Liam <04liagla@skola.boras.se>
2024-10-04 09:12:39 +0200

    added screenshot feature

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,23 +1,22 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as ImagePicker from 'expo-image-picker';
-import React, { useState } from 'react';
+import * as MediaLibrary from 'expo-media-library';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button, FlatList, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { captureRef } from 'react-native-view-shot';
 import { RootStackParamList } from '../App';
 import { SimpleTodo, todos as initialTodos } from '../data';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
-
 const TodoList = ({ navigation, todos }: { navigation: Props['navigation'], todos: SimpleTodo[] }) => {
   const renderItem = ({ item }: { item: SimpleTodo }) => (
     <View style={styles.item}>
-
       <Text>{item.title}</Text>
       <Button
         title="View Details"
         onPress={() => navigation.navigate('DetailsTodo', { ...item })}
       />
-
     </View>
   );
 
@@ -31,45 +30,77 @@ const TodoList = ({ navigation, todos }: { navigation: Props['navigation'], todo
 };
 
 export default function HomeScreen({ navigation }: Props) {
+  const [todos, setTodos] = useState<SimpleTodo[]>(initialTodos); 
+  const [image, setImage] = useState<string | null>(null); 
+  const imageRef = useRef<View>(null); 
 
-    const [expoPushToken, setExpoPushToken] = useState('');
+  const [mediaLibraryPermission, requestPermission] = MediaLibrary.usePermissions();
 
-    const [todos, setTodos] = useState<SimpleTodo[]>(initialTodos); 
-    const [image, setImage] = useState<string | null>(null);
+  useEffect(() => {
+    if (!mediaLibraryPermission?.granted) {
+      requestPermission();
+    }
+  }, [mediaLibraryPermission]);
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
+  const pickImage = () => {
+    ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
+    })
+    .then((result) => {
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
+      }
+    })
+    .catch((e) => {
+      console.log(e);
     });
+  };
 
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
+  const onSaveImageAsync = () => {
+    if (imageRef.current) {
+      captureRef(imageRef.current, {
+        height: 440,
+        quality: 1,
+        format: 'png',
+      })
+      .then((localUri) => {
+        return MediaLibrary.saveToLibraryAsync(localUri);
+      })
+      .then(() => {
+        alert('Screenshot saved!');
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    } else {
+      console.log("imageRef is null or not assigned correctly.");
     }
   };
+
   const handleCreateTodo = (newTodo: SimpleTodo) => {
     setTodos((prevTodos) => [...prevTodos, newTodo]);
   };
 
   return (
-   <View style={{ flex: 1 }}>
-      {image ? (
-        <ImageBackground
-          source={{ uri: image }} 
-          resizeMode="cover"
-          style={styles.image}
-        >
-          <TodoList navigation={navigation} todos={todos} />
-        </ImageBackground>
-      ) : (
-        <View style={styles.noImageContainer}>
-          <TodoList navigation={navigation} todos={todos} />
-        </View>
-      )}
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: 'white' }} ref={imageRef} collapsable={false} >
+        {image ? (
+          <ImageBackground
+            source={{ uri: image }} 
+            resizeMode="cover"
+            style={styles.image}
+          >
+            <TodoList navigation={navigation} todos={todos} />
+          </ImageBackground>
+        ) : (
+          <View style={styles.noImageContainer}>
+            <TodoList navigation={navigation} todos={todos} />
+          </View>
+        )}
+      </View>
       
       <Button
         title="Create Todo"
@@ -78,6 +109,10 @@ export default function HomeScreen({ navigation }: Props) {
       <Button
         title='Set background'
         onPress={pickImage}
+      />
+      <Button
+        title='Take Screenshot'
+        onPress={onSaveImageAsync}
       />
     </View>
   );
